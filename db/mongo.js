ec3cefddbae0bfbf69dd2c5cdd121ade899cc9ee
yeswanth <yeswanth15750@gmail.com>
2023-07-03 13:52:31 +0530

    s

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,7 @@ const uri = `mongodb+srv://admin123:admin123@training.xr6wcyd.mongodb.net/?retry
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri,{
     serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true }
-});
+}); 
 
 async function run() {
     try {
